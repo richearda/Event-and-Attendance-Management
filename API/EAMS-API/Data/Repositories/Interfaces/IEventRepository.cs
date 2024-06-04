600f7f4c7c4835dc69751352bda82c7a815c6c62
Riche <richearda1996@gmail.com>
2024-06-04 23:05:39 +0800

    Add repositories

---
+++ API/EAMS-API/Data/Repositories/Interfaces/IEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETMS_API.Models;
+
+namespace ETMS_API.Data.Repositories.Interfaces
+{
+    public interface IEventRepository
+    {
+        Task<Event> GetByIdAsync(int id);
+        Task<List<Event>> GetEvents();
+        Task<Event> AddEvent(Event @event);
+        Task<Event> UpdateEvent(Event @event);
+        void DeleteEvent(Event @event);
+    }
+}
