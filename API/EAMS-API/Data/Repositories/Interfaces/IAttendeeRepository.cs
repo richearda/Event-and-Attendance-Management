78b8ba33f5c9e1263af8acac744a4fc8ba9d3634
Riche <richearda1996@gmail.com>
2024-06-05 20:27:15 +0800

    Added method signatures for Attendee repository interface

--- API/EAMS-API/Data/Repositories/Interfaces/IAttendeeRepository.cs
+++ API/EAMS-API/Data/Repositories/Interfaces/IAttendeeRepository.cs
@@ -4,12 +4,14 @@ namespace ETMS_API.Data.Repositories.Interfaces
 {
     public interface IAttendeeRepository
     {
-        Task<Attendee> GetByIdAsync(int id);
+        Task<Attendee> GetByIdAsync(int attendeeId);
         Task<IEnumerable<Attendee>> GetAttendeesAsync();
-        Task<IEnumerable<Event>> GetAttendedEventsAsync(int id);
-        Task<IEnumerable<Feedback>> GetAttendeeFeedbacksAsync(int id);
+        Task<IEnumerable<Event>> GetAttendedEventsAsync(int attendeeId);
+        Task<IEnumerable<Feedback>> GetAttendeeFeedbacksAsync(int attendeeId);
         Task<Attendee> AddAttendee(Attendee attendee);
         Task<Attendee> UpdateAttendee(Attendee attendee);
         void DeleteAttendee(Attendee attendee);
+        Task CheckInAsync(int attendeeId);
+        Task CheckOutAsync(int attendeeId);
     }
 }
