65b63debaa4ccd030594f0c8c7166d9687ab59f0
Riche <richearda1996@gmail.com>
2024-06-05 22:01:59 +0800

    Added feedback repository interface and methods

---
+++ API/EAMS-API/Data/Repositories/Interfaces/IFeedbackRepository.cs
@@ -0,0 +1,15 @@
+﻿using ETMS_API.Models;
+
+namespace ETMS_API.Data.Repositories.Interfaces
+{
+    public interface IFeedbackRepository
+    {
+        Task<Feedback> GetByIdAsync(int feedbackId);
+        Task<IEnumerable<Feedback>> GetFeedbacksAsync();
+        Task AddFeedbackAsync(Feedback feedback);
+        Task UpdateFeedbackAsync(Feedback feedback);
+        Task DeleteFeedbackAsync(int feedbackId);
+        Task<IEnumerable<Feedback>> GetFeedbackByEventAsync(int eventId);
+        Task<IEnumerable<Feedback>> GetFeedbackByAttendeeAsync(int attendeeId);
+    }
+}
