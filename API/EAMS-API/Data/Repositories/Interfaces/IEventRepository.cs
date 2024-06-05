d5d79034dd2a0c2d36ba77352315f98932cafc6e
Riche <richearda1996@gmail.com>
2024-06-05 21:24:36 +0800

    Modify and added method signatures for event repository interface

--- API/EAMS-API/Data/Repositories/Interfaces/IEventRepository.cs
+++ API/EAMS-API/Data/Repositories/Interfaces/IEventRepository.cs
@@ -4,10 +4,14 @@ namespace ETMS_API.Data.Repositories.Interfaces
 {
     public interface IEventRepository
     {
-        Task<Event> GetByIdAsync(int id);
+        Task<Event> GetByIdAsync(int eventId);
         Task<IEnumerable<Event>> GetEvents();
-        Task<IEnumerable<Attendee>> GetEventAttendees(int id);
-        Task<IEnumerable<Feedback>> GetEventFeedbacks(int id);
+        Task<IEnumerable<Attendee>> GetEventAttendees(int eventId);
+        Task<IEnumerable<Feedback>> GetEventFeedbacksAsync(int eventId);
+        Task<IEnumerable<Event>> GetUpcomingEventsAsync();
+        Task<IEnumerable<Event>> GetPastEventsAsync();
+        Task<bool> RegisterForEventAsync(int eventId, string userId);
+        Task AddFeedbackAsync(int eventId, string userId, string comment, int rating);
         Task<Event> AddEvent(Event @event);
         Task<Event> UpdateEvent(Event @event);
         void DeleteEvent(Event @event);
