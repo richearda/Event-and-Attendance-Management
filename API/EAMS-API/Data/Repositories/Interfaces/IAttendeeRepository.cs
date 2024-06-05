d1af979508ae490cad46fd43cd54534f929bdcd5
Riche Arda <richearda1996@gmail.com>
2024-06-05 12:38:51 +0800

    Added interface repository for attendee

---
+++ API/EAMS-API/Data/Repositories/Interfaces/IAttendeeRepository.cs
@@ -0,0 +1,15 @@
+using ETMS_API.Models;
+
+namespace ETMS_API.Data.Repositories.Interfaces
+{
+    public interface IAttendeeRepository
+    {
+        Task<Attendee> GetByIdAsync(int id);
+        Task<List<Attendee>> GetAttendeesAsync();
+        Task<List<Event> GetAttendedEventsAsync(int id);
+        Task<List<Feedback> GetAttendeeFeedbacksAsync(int id);
+        Task<Attendee> AddAttendee(Attendee attendee);
+        Task<Attendee> UpdateAttendee(Attendee attendee);
+        void DeleteAttendee(Attendee attendee);
+    }
+}
