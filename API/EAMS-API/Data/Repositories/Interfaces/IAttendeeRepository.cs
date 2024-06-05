f6af588ec9959cd74550603c36bc6ff95cbcdc86
Riche <richearda1996@gmail.com>
2024-06-05 20:00:25 +0800

    Corrected the syntax

--- API/EAMS-API/Data/Repositories/Interfaces/IAttendeeRepository.cs
+++ API/EAMS-API/Data/Repositories/Interfaces/IAttendeeRepository.cs
@@ -6,8 +6,8 @@ namespace ETMS_API.Data.Repositories.Interfaces
     {
         Task<Attendee> GetByIdAsync(int id);
         Task<List<Attendee>> GetAttendeesAsync();
-        Task<List<Event> GetAttendedEventsAsync(int id);
-        Task<List<Feedback> GetAttendeeFeedbacksAsync(int id);
+        Task<List<Event>> GetAttendedEventsAsync(int id);
+        Task<List<Feedback>> GetAttendeeFeedbacksAsync(int id);
         Task<Attendee> AddAttendee(Attendee attendee);
         Task<Attendee> UpdateAttendee(Attendee attendee);
         void DeleteAttendee(Attendee attendee);
--- API/EAMS-API/Data/Repositories/Interfaces/IEventRepository.cs
+++ API/EAMS-API/Data/Repositories/Interfaces/IEventRepository.cs
@@ -6,8 +6,8 @@ namespace ETMS_API.Data.Repositories.Interfaces
     {
         Task<Event> GetByIdAsync(int id);
         Task<List<Event>> GetEvents();
-        Task<List<Attendee> GetEventAttendees(int id);
-        Task<List<Feedback> GetEventFeedbacks(int id);
+        Task<List<Attendee>> GetEventAttendees(int id);
+        Task<List<Feedback>> GetEventFeedbacks(int id);
         Task<Event> AddEvent(Event @event);
         Task<Event> UpdateEvent(Event @event);
         void DeleteEvent(Event @event);
