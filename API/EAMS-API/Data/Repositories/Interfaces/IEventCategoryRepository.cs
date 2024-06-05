96401d3e9f65384099a069358ee80e2998a31376
Riche <richearda1996@gmail.com>
2024-06-05 22:07:09 +0800

    Added event category repository interface and method signatures

---
+++ API/EAMS-API/Data/Repositories/Interfaces/IEventCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETMS_API.Models;
+
+namespace ETMS_API.Data.Repositories.Interfaces
+{
+    public interface IEventCategoryRepository
+    {
+        Task<IEnumerable<EventCategory>> GetAllCategoriesAsync();
+        Task<EventCategory> GetCategoryByIdAsync(int categoryId);
+        Task AddCategoryAsync(EventCategory category);
+        Task UpdateCategoryAsync(EventCategory category);
+        Task DeleteCategoryAsync(int categoryId);
+    }
+}
