2fb197bb619e9a6b86f5e7f21d940324558496f9
Riche <richearda1996@gmail.com>
2024-06-08 19:40:50 +0800

    Added interface repository implementation for event category mapping

---
+++ API/EAMS-API/Data/Repositories/EventCategoryMappingRepository.cs
@@ -0,0 +1,45 @@
+﻿using ETMS_API.Data.Repositories.Interfaces;
+using ETMS_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETMS_API.Data.Repositories
+{
+    public class EventCategoryMappingRepository : IEventCategoryMappingRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public EventCategoryMappingRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<EventCategoryMapping> AddEventCategoryMapping(EventCategoryMapping eventCategoryMapping)
+        {
+            await _dbContext.EventCategoriesMappings.AddAsync(eventCategoryMapping);
+            await _dbContext.SaveChangesAsync();
+            return eventCategoryMapping;
+        }
+
+        public void DeleteEventCategoryMapping(EventCategoryMapping eventCategoryMapping)
+        {
+            _dbContext.EventCategoriesMappings.Remove(eventCategoryMapping);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task<EventCategoryMapping> GetByIdAsync(int id)
+        {
+            return await _dbContext.EventCategoriesMappings.FindAsync(id);
+
+        }
+
+        public async Task<IEnumerable<EventCategoryMapping>> GetEventCategoriesAsync()
+        {
+            return await _dbContext.EventCategoriesMappings.ToListAsync();
+        }
+
+        public async Task<EventCategoryMapping> UpdateEventCategoryMapping(EventCategoryMapping eventCategoryMapping)
+        {
+            _dbContext.Entry(eventCategoryMapping).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return eventCategoryMapping;
+        }
+    }
+}
