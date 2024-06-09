a1034b52d2668fcacfc5c5dadbe0f9ae1173218a
Riche <richearda1996@gmail.com>
2024-06-09 21:30:16 +0800

    Add controllers for Event and Categories
    Add EventsController with CreateEvent HttpPost method
    
    Add CategoriesController with CreateEventCategory HttpPost method

---
+++ API/EAMS-API/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using ETMS_API.Data.Repositories.Interfaces;
+using ETMS_API.DTOs.EventCategory;
+using ETMS_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETMS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class CategoriesController : ControllerBase
+    {
+        private IEventCategoryRepository _eventCategoryRepository;
+        private IMapper _mapper;
+        public CategoriesController(IEventCategoryRepository eventCategoryRepository, IMapper mapper)
+        {
+            _eventCategoryRepository = eventCategoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEventCategory([FromBody] CreateEventCategoryDto categoryDto)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var categoryToAdd = _mapper.Map<EventCategory>(categoryDto);
+            await _eventCategoryRepository.AddCategoryAsync(categoryToAdd);
+            return Ok(categoryToAdd);
+        }
+
+    }
+}
---
+++ API/EAMS-API/Controllers/EventsController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using ETMS_API.Data.Repositories.Interfaces;
+using ETMS_API.DTOs.Event;
+using ETMS_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETMS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+        private readonly IMapper _mapper;
+        public EventsController(IEventRepository eventRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEvent([FromBody] CreateEventDto eventDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var eventToAdd = _mapper.Map<Event>(eventDto);
+            await _eventRepository.AddEventAsync(eventToAdd);
+            return Ok();
+        }
+
+
+    }
+}
