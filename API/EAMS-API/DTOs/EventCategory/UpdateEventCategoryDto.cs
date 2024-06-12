b087c0c1b6f5be23e42a326d48970f21424a0e7f
Riche <richearda1996@gmail.com>
2024-06-12 20:03:17 +0800

    Add UpdateEventCategoryDto

---
+++ API/EAMS-API/DTOs/EventCategory/UpdateEventCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ETMS_API.DTOs.EventCategory
+{
+    public class UpdateEventCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
