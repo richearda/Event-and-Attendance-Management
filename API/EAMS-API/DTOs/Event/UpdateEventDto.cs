a851790d82a5b4b2c51711752198c829812bad23
Riche <richearda1996@gmail.com>
2024-06-12 19:22:49 +0800

    Modify and add DTO for Event

---
+++ API/EAMS-API/DTOs/Event/UpdateEventDto.cs
@@ -0,0 +1,16 @@
+﻿using ETMS_API.DTOs.EventCategory;
+
+namespace ETMS_API.DTOs.Event
+{
+    public class UpdateEventDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Location { get; set; }
+        public string OrganizerId { get; set; }
+        public int Capacity { get; set; }        
+        public UpdateEventCategoryMappingDto EventCategory { get; set; }
+    }
+}
---
+++ API/EAMS-API/DTOs/EventCategory/UpdateEventCategoryMappingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ETMS_API.DTOs.EventCategory
+{
+    public class UpdateEventCategoryMappingDto
+    {
+        public int EventCategoryMappingId { get; set; }
+        public int EventId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
