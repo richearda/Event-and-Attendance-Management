f7cfeed9604921a8b106ff305063a5e6baea2964
Riche <richearda1996@gmail.com>
2024-07-01 21:59:55 +0800

    Update the migrations for adding columns for tables

--- API/EAMS-API/Data/Migrations/20240701132526_Add columns to table AspNetUsers and Attendees.Designer.cs
+++ API/EAMS-API/Data/Migrations/20240701132526_Add columns to table AspNetUsers and Attendees.Designer.cs
@@ -4,6 +4,7 @@ using ETMS_API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ETMS_API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240701132526_Add columns to table AspNetUsers and Attendees")]
+    partial class AddcolumnstotableAspNetUsersandAttendees
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/EAMS-API/Data/Migrations/20240701132526_Add columns to table AspNetUsers and Attendees.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETMS_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddcolumnstotableAspNetUsersandAttendees : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendees_AspNetUsers_UserId",
+                table: "Attendees");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckOutTime",
+                table: "Attendees",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendees_AspNetUsers_UserId",
+                table: "Attendees",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendees_AspNetUsers_UserId",
+                table: "Attendees");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutTime",
+                table: "Attendees");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendees_AspNetUsers_UserId",
+                table: "Attendees",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
