a3eb42e8071b8ea5cd7e01ef860cc9f4eea86958
Riche <richearda1996@gmail.com>
2024-07-04 20:48:48 +0800

    Add AttendeeDto

---
+++ API/EAMS-API/DTOs/Attendee/AttendeeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ETMS_API.DTOs.Attendee
+{
+    public class AttendeeDto
+    {
+        public int AttendeeId { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public DateTime? CheckInTime { get; set; }
+        public DateTime? CheckOutTime { get; set; }
+    }
+}
