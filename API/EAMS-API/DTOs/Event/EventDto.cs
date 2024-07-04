07d09f454267a1c5e2fde393a9b4f1e5e36c1e48
Riche <richearda1996@gmail.com>
2024-07-04 20:44:38 +0800

    Add EventDto class

---
+++ API/EAMS-API/DTOs/Event/EventDto.cs
@@ -0,0 +1,16 @@
+﻿using ETMS_API.DTOs.Attendee;
+
+namespace ETMS_API.DTOs.Event
+{
+    public class EventDto
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public string Location { get; set; }
+        public string Organizer { get; set; }
+        public ICollection<AttendeeDto> Attendees { get; set; }
+    }
+}
