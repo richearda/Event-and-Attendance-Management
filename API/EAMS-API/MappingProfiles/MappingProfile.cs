895b4c5f158b3da5cfad8ae6ba14a149ce6dd1d8
Riche <richearda1996@gmail.com>
2024-07-07 14:35:11 +0800

    Modify mapping for attendee dto's for child properties

--- API/EAMS-API/MappingProfiles/MappingProfile.cs
+++ API/EAMS-API/MappingProfiles/MappingProfile.cs
@@ -27,17 +27,17 @@ namespace ETMS_API.MappingProfiles
             CreateMap<AttendedEventsDto,Attendee>()
                 .ForMember(dest => dest.AttendeeId, opt => opt.MapFrom(src => src.AttendeeId))
                 .ForMember(dest => dest.EventId, opt => opt.MapFrom(src => src.EventId))
-                .ForMember(dest => dest.Event.Title, opt => opt.MapFrom(src => src.EventTitle))
-                .ForMember(dest => dest.Event.Description, opt => opt.MapFrom(src => src.EventDescription))
-                .ForMember(dest => dest.Event.Date, opt => opt.MapFrom(src => src.EventDate))
-                .ForMember(dest => dest.Event.Location, opt => opt.MapFrom(src => src.Location))
-                .ForMember(dest => dest.Event.Organizer, opt => opt.MapFrom(src => src.Organizer))
+                .ForPath(dest => dest.Event.Title, opt => opt.MapFrom(src => src.EventTitle))
+                .ForPath(dest => dest.Event.Description, opt => opt.MapFrom(src => src.EventDescription))
+                .ForPath(dest => dest.Event.Date, opt => opt.MapFrom(src => src.EventDate))
+                .ForPath(dest => dest.Event.Location, opt => opt.MapFrom(src => src.Location))
+                .ForPath(dest => dest.Event.Organizer, opt => opt.MapFrom(src => src.Organizer))
                 .ForMember(dest => dest.CheckInTime, opt => opt.MapFrom(src => src.CheckInTime))
                 .ForMember(dest => dest.CheckOutTime, opt => opt.MapFrom(src => src.CheckOutTime))
                 .ReverseMap();
             CreateMap<AttendeeDto, Attendee>()
-                .ForMember(dest => dest.User.FullName, opt => opt.MapFrom(src => src.FullName))
-                .ForMember(dest => dest.User.Address, opt => opt.MapFrom(src => src.Address))
+                .ForPath(dest => dest.User.FullName, opt => opt.MapFrom(src => src.FullName))
+                .ForPath(dest => dest.User.Address, opt => opt.MapFrom(src => src.Address))
                 .ReverseMap();
                 
         }
