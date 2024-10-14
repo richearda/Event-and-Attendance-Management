48010d40225ef527f9f2014a3e9edb35ce5a9114
Riche <richearda1996@gmail.com>
2024-10-14 17:30:19 +0800

    Add summary description for each feedbacks endpoint.

--- API/EAMS-API/Controllers/FeedbacksController.cs
+++ API/EAMS-API/Controllers/FeedbacksController.cs
@@ -19,13 +19,18 @@ namespace ETMS_API.Controllers
             _feedbackRepository = feedbackRepository;
             _mapper = mapper;
         }
-
+        /// <summary>
+        /// Get all feedbacks
+        /// </summary>
         [HttpGet]
         public async Task<IActionResult> GetFeedbacks()
         {
             var res = await _feedbackRepository.GetFeedbacksAsync();
             return Ok(res);
         }
+        /// <summary>
+        /// Get feedback by id
+        /// </summary>
         [HttpGet]
         [Route("{id}")]
         public async Task<IActionResult> GetFeedback(int id)
@@ -33,6 +38,9 @@ namespace ETMS_API.Controllers
             var res = await _feedbackRepository.GetByIdAsync(id);
             return Ok(res);
         }
+        /// <summary>
+        /// Get feedback by event id
+        /// </summary>
         [HttpGet]
         [Route("event/{id}")]
         public async Task<IActionResult> GetFeedbackByEvent(int id)
@@ -40,6 +48,10 @@ namespace ETMS_API.Controllers
             var res = await _feedbackRepository.GetFeedbackByEventAsync(id);
             return Ok(res);
         }
+        /// <summary>
+        /// Get feedback by attendee id
+        /// </summary>
+
         [HttpGet]
         [Route("attendee/{id}")]
         public async Task<IActionResult> GetFeedbackByAttendee(int id)
@@ -47,18 +59,27 @@ namespace ETMS_API.Controllers
             var res = await _feedbackRepository.GetFeedbackByAttendeeAsync(id);
             return Ok(res);
         }
+        /// <summary>
+        /// Add a feedback
+        /// </summary>
         [HttpPost]
         public async Task<IActionResult> AddFeedback(Feedback feedback)
         {
             var res = _feedbackRepository.AddFeedbackAsync(feedback);
             return Ok(res);
         }
+        /// <summary>
+        /// Update a feedback
+        /// </summary>
         [HttpPut]
         public async Task<IActionResult> UpdateFeedback(Feedback feedback)
         {
             var res = _feedbackRepository.UpdateFeedbackAsync(feedback);
             return Ok(res);
         }
+        /// <summary>
+        /// Delete a feedback by id
+        /// </summary>
         [HttpDelete]
         [Route("{id}")]
         public async Task<IActionResult> DeleteFeedback(int id)
