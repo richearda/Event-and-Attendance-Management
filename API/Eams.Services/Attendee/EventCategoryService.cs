9a2ce83b83bf4b4363c267687bc6167773821271
Riche <richearda1996@gmail.com>
2024-10-30 20:25:35 +0800

    Add service for Event Category

---
+++ API/Eams.Services/Attendee/EventCategoryService.cs
@@ -0,0 +1,44 @@
+﻿using Eams.Core.Domain;
+using Eams.Data.Repositories.Interfaces;
+using Eams.Services.Attendee.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eams.Services.Attendee
+{
+    public class EventCategoryService : IEventCategoryService
+    {
+        private readonly IEventCategoryRepository _eventCategoryRepository;
+        public EventCategoryService(IEventCategoryRepository eventCategoryRepository)
+        {
+            _eventCategoryRepository = eventCategoryRepository;
+        }
+        public async Task AddCategoryAsync(EventCategory category)
+        {
+            await _eventCategoryRepository.AddCategoryAsync(category);
+        }
+
+        public async Task DeleteCategoryAsync(int categoryId)
+        {
+            await _eventCategoryRepository.DeleteCategoryAsync(categoryId);
+        }
+
+        public async Task<IEnumerable<EventCategory>> GetAllCategoriesAsync()
+        {
+            return await _eventCategoryRepository.GetAllCategoriesAsync();
+        }
+
+        public async Task<EventCategory> GetCategoryByIdAsync(int categoryId)
+        {
+            return await _eventCategoryRepository.GetCategoryByIdAsync(categoryId);
+        }
+
+        public async Task UpdateCategoryAsync(int id, EventCategory category)
+        {
+            await _eventCategoryRepository.UpdateCategoryAsync(id, category);
+        }
+    }
+}
---
+++ API/Eams.Services/Attendee/Interfaces/IEventCategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Eams.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eams.Services.Attendee.Interfaces
+{
+    public interface IEventCategoryService
+    {
+        Task<IEnumerable<EventCategory>> GetAllCategoriesAsync();
+        Task<EventCategory> GetCategoryByIdAsync(int categoryId);
+        Task AddCategoryAsync(EventCategory category);
+        Task UpdateCategoryAsync(int id, EventCategory category);
+        Task DeleteCategoryAsync(int categoryId);
+    }
+}
